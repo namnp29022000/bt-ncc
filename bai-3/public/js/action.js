2033fa1dfe591b298b170247a0234ba84ce86714
NamNguyenNhuong <namnp2902@gmail.com>
2019-08-08 13:49:01 +0700

    bai3

---
+++ bai-3/public/js/action.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").toggle();
+    });
+  });
+//   $("#flip").click(function(){
+//     $("#panel").animate({width: 'toggle'});
+// });
